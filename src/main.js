c06a0b0c82cbcc1fef1158e5b6a20abe2a8acf3c
Max <f_mb@live.dk>
2025-03-13 07:41:29 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -8,8 +8,9 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 // Variables
 let hoveredObject = null
 
-let sampleRoomMesh = null
+let room1Mesh = null
 let sampleBoxMesh = null
+let sampleIcosphereMesh = null
 
 let isInRoom = false
 let isMoving = false
@@ -20,7 +21,8 @@ let red = new THREE.Color(0xff0000)
 let beigeYellow = new THREE.Color(0xfff1ab)
 let originalColor = new THREE.Color() // Variable to save whatever color something is
 let originalColorRoom = new THREE.Color();
-let originalColorBox = new THREE.Color(); 
+let originalColorBox = new THREE.Color();
+let originalColorIcosphere = new THREE.Color(); 
 
 // Sizes
 const sizes = {
@@ -59,25 +61,40 @@ controls.enableZoom = false
 const boxGeometry = new THREE.BoxGeometry(10, 10, 10)
 
 // Models
+const ORIGINAL_COLORS = new Map() // What is a map?
+
+function SaveOriginalColor(object)
+{
+  if (object.material && object.material.color)
+  {
+    ORIGINAL_COLORS.set(object, object.material.color.clone())
+  }
+}
+
 const loader = new GLTFLoader();
-loader.load('/Models/SampleRoom.glb', function(gltf) {
+loader.load('/Models/Room1.glb', function(gltf)
+{
     const sampleRoom = gltf.scene
-    
-    // const sampleRoomMesh = sampleRoom.getObjectByName('SampleRoom')
-    // const sampleBoxMesh = sampleRoom.getObjectByName('SampleBox')
-    
-    sampleRoom.traverse((child) =>{
-        if (child.isMesh){
-          if (child.name === 'SampleRoom'){
-          sampleRoomMesh = child;
-          originalColorRoom.copy(sampleRoomMesh.material.color)
-          }
-          if (child.name === "SampleCube"){
-            sampleBoxMesh = child;
-            originalColorBox.copy(sampleBoxMesh.material.color)
+
+    // Using the names given to the models in the blender file. Remember to name things appropriately in there!
+    const rooms = [];
+    const items = [];
+    sampleRoom.traverse((child) =>
+    {
+      if (child.isMesh)
+      {
+        if (child.name.startsWith('Room_'))
+        {
+          rooms.push(child)
+          SaveOriginalColor(child)
+        }
+        else if (child.name.startsWith('Item_'))
+        {
+          items.push(child)
+          SaveOriginalColor(child)
         }
       }
-    });
+    })
 
     console.log(sampleRoom.children);
     scene.add(sampleRoom)
@@ -132,62 +149,119 @@ const loop = () => {
 loop();
 
 // Raycast selector renderer - I need to change the sample meshes to tags or something at some point.
-function render() {
-  raycaster.setFromCamera(pointer, camera);
+// function render() {
+//   raycaster.setFromCamera(pointer, camera);
 
-  const intersects = raycaster.intersectObjects(scene.children, true);
+//   const INTERSECTS = raycaster.intersectObjects(scene.children, true);
 
-  if (intersects.length > 0 && isMoving === false) {
-    const object = intersects[0].object;
+//   if (INTERSECTS.length > 0 && isMoving === false) {
+//     const OBJECT = INTERSECTS[0].object;
 
-    if (object === sampleRoomMesh) {
-      if (isInRoom === false) {
-        if (hoveredObject !== sampleRoomMesh) {
-          if (hoveredObject) {
-            if (hoveredObject.material && hoveredObject.material.color) {
-              hoveredObject.material.color.copy(
-                hoveredObject === sampleBoxMesh ? originalColorBox : originalColorRoom
-              );
-            }
-          }
-          hoveredObject = sampleRoomMesh;
-          sampleRoomMesh.material.color.set(beigeYellow);
-        }
-      } else {
-        if (hoveredObject === sampleBoxMesh) {
-          if (hoveredObject.material && hoveredObject.material.color) {
-            hoveredObject.material.color.copy(originalColorBox);
-          }
-          hoveredObject = null;
-        }
-      }
-    } else if (object === sampleBoxMesh && isInRoom === true) {
-      if (hoveredObject !== sampleBoxMesh) {
-        if (hoveredObject) {
-          if (hoveredObject.material && hoveredObject.material.color) {
-            hoveredObject.material.color.copy(originalColorRoom);
-          }
-        }
-        hoveredObject = sampleBoxMesh;
-        sampleBoxMesh.material.color.set(beigeYellow);
-      }
+//     if (OBJECT === room1Mesh) {
+//       if (isInRoom === false) {
+//         if (hoveredObject !== room1Mesh) {
+//           if (hoveredObject) {
+//             if (hoveredObject.material && hoveredObject.material.color)
+//               {
+//               hoveredObject.material.color.copy
+//               (
+//                 hoveredObject === sampleBoxMesh ? originalColorBox : originalColorRoom
+//               );
+//             }
+//           }
+//           hoveredObject = room1Mesh;
+//           room1Mesh.material.color.set(beigeYellow);
+//         }
+//       } else {
+//         if (hoveredObject === sampleBoxMesh) {
+//           if (hoveredObject.material && hoveredObject.material.color) {
+//             hoveredObject.material.color.copy(originalColorBox);
+//           }
+//           hoveredObject = null;
+//         }
+//       }
+//     } else if (OBJECT === sampleBoxMesh && isInRoom === true) {
+//       if (hoveredObject !== sampleBoxMesh) {
+//         if (hoveredObject) {
+//           if (hoveredObject.material && hoveredObject.material.color) {
+//             hoveredObject.material.color.copy(originalColorRoom);
+//           }
+//         }
+//         hoveredObject = sampleBoxMesh;
+//         sampleBoxMesh.material.color.set(beigeYellow);
+//       }
+//     }
+//   } else {
+//     if (hoveredObject) {
+//       if (hoveredObject.material && hoveredObject.material.color) {
+//         hoveredObject.material.color.copy(
+//           hoveredObject === room1Mesh ? originalColorRoom : originalColorBox
+//         );
+//       }
+//       hoveredObject = null;
+//     }
+//   }
+
+//   renderer.render(scene, camera);
+// }
+
+function Render()
+{
+  raycaster.setFromCamera(pointer, camera)
+  const INTERSECTS = raycaster.intersectObjects(scene.children, true)
+
+  if (INTERSECTS.length > 0 && !isMoving)
+    {
+    const OBJECT = INTERSECTS[0].object;
+
+    if ((OBJECT.name.startsWith('Room_') && !isInRoom) || (OBJECT.name.startsWith('Item_') && isInRoom))
+    {
+      HandleHover(OBJECT, OBJECT);
     }
-  } else {
-    if (hoveredObject) {
-      if (hoveredObject.material && hoveredObject.material.color) {
-        hoveredObject.material.color.copy(
-          hoveredObject === sampleRoomMesh ? originalColorRoom : originalColorBox
-        );
+    else
+    {
+      if ((OBJECT.name.startsWith('Room_') && isInRoom) || (OBJECT.name.startsWith('Item_') && !isInRoom))
+      {
+        ResetHoveredObject();
       }
-      hoveredObject = null;
     }
   }
+  else
+  {
+    ResetHoveredObject();
+  }
 
-  renderer.render(scene, camera);
+  renderer.render(scene, camera)
+}
+
+function ResetHoveredObject()
+{
+  if (hoveredObject && ORIGINAL_COLORS.has(hoveredObject))
+  {
+    hoveredObject.material.color.copy(ORIGINAL_COLORS.get(hoveredObject))
+    hoveredObject = null
+  }
+}
+
+function HandleHover(intersectedObject, targetObject)
+{
+  if (intersectedObject === targetObject)
+  {
+    if (hoveredObject !== targetObject)
+    {
+      ResetHoveredObject()
+      hoveredObject = targetObject
+      targetObject.material.color.set(beigeYellow)
+    }
+  }
+  else
+  {
+    ResetHoveredObject()
+  }
 }
 
 // Calling the render function
-render();
+Render();
 
 // Update pointer position on mouse move
 window.addEventListener('pointermove', (event) => {
@@ -199,7 +273,7 @@ function onPointerMove(event) {
   pointer.x = (event.clientX / sizes.width) * 2 - 1;
   pointer.y = -(event.clientY / sizes.height) * 2 + 1;
 
-  render();
+  Render();
 }
 window.addEventListener( 'pointermove', onPointerMove );
 
@@ -208,13 +282,13 @@ function onPointerClick (event) {
   pointer.y = -(event.clientY / sizes.height) * 2 + 1;
 
   raycaster.setFromCamera(pointer, camera)
-  const intersects = raycaster.intersectObjects(scene.children, true)
+  const INTERSECTS = raycaster.intersectObjects(scene.children, true)
 
-  if (intersects.length > 0) {
-    const object = intersects[0].object
+  if (INTERSECTS.length > 0) {
+    const OBJECT = INTERSECTS[0].object
 
-    if (object === sampleRoomMesh && isInRoom === false) {
-      sampleRoomMesh.material.color.set(originalColorRoom)
+    if (OBJECT.name.startsWith('Room_') && isInRoom === false) {
+      OBJECT.material.color.set(ORIGINAL_COLORS.get(OBJECT))
       isInRoom = true
 
       controls.enabled = false
